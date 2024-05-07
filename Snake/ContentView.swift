95966cb518ddcdc3ea740a24920be9baaf0cf026
Denis Denisov <denis.denisov8701@gmail.com>
2024-05-07 21:28:50 +0200

    with out button

--- Snake/ContentView.swift
+++ Snake/ContentView.swift
@@ -9,51 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isDay = true
+    @State private var isButtonDisabled = false
+    
     @State private var sunPosition = 0.0
-    @State private var moonPosition = 0.0
+    @State private var moonPosition = -400.0
     
     var body: some View {
-        GeometryReader { geometry in
-            let width = geometry.size.width
-            let height = geometry.size.height
+        ZStack {
+            Color(isDay ? .blue : .black).ignoresSafeArea()
             
-            ZStack {
-                Color(isDay ? .blue : .black).ignoresSafeArea()
-                
-                SunView()
-                    .frame(width: 200, height: 200)
-                    .offset(x: sunPosition, y: 0)
-                    .opacity(isDay ? 1 : 0)
-                
-                MoonView()
-                    .frame(width: 200, height: 200)
-                    .offset(x: moonPosition, y: 0)
-                    .opacity(isDay ? 0 : 1)
-            }
+            SunView()
+                .frame(width: 200, height: 200)
+                .offset(x: sunPosition, y: 0)
+                .opacity(isDay ? 1 : 0)
             
-            Button(isDay ? "Go sleep" : "Wake Up") {
-                withAnimation(.easeInOut(duration: 1)) {
-                    isDay.toggle()
-                    if isDay {
-                        sunPosition = 0
-                        moonPosition = 0 + 100
-                        
-                    } else {
-                        moonPosition = 0
-                        sunPosition = 0 + 100
-                    }
-                }
-                if isDay {
-                    moonPosition = -width / 2 - 100
-                } else {
-                    sunPosition = -width / 2 - 100
-                }
+            MoonView()
+                .frame(width: 200, height: 200)
+                .offset(x: moonPosition, y: 0)
+                .opacity(isDay ? 0 : 1)
+        }
+    }
+    
+    private func startAnimation() {
+        isButtonDisabled.toggle()
+        
+        withAnimation(.easeInOut(duration: 2)) {
+            isDay.toggle()
+            if isDay {
+                sunPosition = 0
+                moonPosition = 400
+            } else {
+                moonPosition = 0
+                sunPosition = 400
             }
-            .padding()
-            .background(.white)
-            .cornerRadius(10)
-            .shadow(radius: 5)
-            .frame(width: width, height: height, alignment: .bottom)
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            if isDay {
+                moonPosition = -400
+            } else {
+                sunPosition = -400
+            }
+            isButtonDisabled.toggle()
         }
     }
 }
