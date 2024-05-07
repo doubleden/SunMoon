9ca2f30c0c98f18600022c20b8b5d1d2882a74ba
Denis Denisov <denis.denisov8701@gmail.com>
2024-05-07 19:18:08 +0200

    Initial Commit

---
+++ Snake/SnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeApp.swift
+//  Snake
+//
+//  Created by Denis Denisov on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
